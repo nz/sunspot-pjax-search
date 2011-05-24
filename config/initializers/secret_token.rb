5866c8aef0308473cd1b910eda0ba733790da612
Nick Zadrozny <nick@beyondthepath.com>
2011-05-24 14:39:50 -0700

    Initial creation.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SunspotPjaxSearch::Application.config.secret_token = '2e10d39ba392988eb7a0b3ad8b2a3a371ea2536f789a61976d86d89478c0a2dea926f8eddd31e93e0758fbd4abbd3e1ff599b8854ed28b7c5a334bfa2acef5ec'
